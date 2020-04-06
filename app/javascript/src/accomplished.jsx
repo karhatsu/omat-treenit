b7a08f8a91d2ca7fe694e6c8ab5f4f70a5387065
Henri Karhatsu <henri@karhatsu.com>
2020-04-06 22:21:15 +0300

    Use emojis for liking

--- app/javascript/src/accomplished.jsx
+++ app/javascript/src/accomplished.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 
 import './accomplished.scss'
 
+const likingEmoji = liking => {
+  switch (liking) {
+    case 2:
+      return '😀'
+    case 1:
+      return '🙂'
+    case 0:
+      return '😕'
+    case -1:
+      return '😠'
+    default:
+      return ''
+  }
+}
+
 function Accomplished({ accomplishment }) {
   const { comment, liking } = accomplishment
   return (
     <div className="accomplished">
       <div className="accomplished__title">Haaste suoritettu!</div>
       <div className="accomplished__feedback">
-        {liking && <div className="accomplished__liking">{liking}</div>}
+        <div className="accomplished__liking">{likingEmoji(liking)}</div>
         {comment && <div className="accomplished__comment">{comment}</div>}
       </div>
     </div>
