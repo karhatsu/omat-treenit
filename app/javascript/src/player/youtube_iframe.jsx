c6920bd99142efd678c3abd071248eaa39915711
Henri Karhatsu <henri@karhatsu.com>
2020-04-07 16:06:54 +0300

    Accept shortened YouTube URLs

--- app/javascript/src/player/youtube_iframe.jsx
+++ app/javascript/src/player/youtube_iframe.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
+const resolveEmbedUrl = url => {
+  if (url.indexOf('youtu.be/') !== -1) {
+    return url.replace('youtu.be/', 'youtube.com/embed/')
+  } else if (url.indexOf('watch?v=') !== -1) {
+    return url.replace('watch?v=', 'embed/')
+  }
+}
+
 function YoutubeIframe({ url }) {
-  if (url.indexOf('watch?v=') === -1) {
+  const embedUrl = resolveEmbedUrl(url)
+  if (!embedUrl) {
     return <a className="youtube-link" href={url} target="_blank">Katso YouTube-video</a>
   }
-  const embedUrl = url.replace('watch?v=', 'embed/')
   return <iframe src={embedUrl} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 }
 
