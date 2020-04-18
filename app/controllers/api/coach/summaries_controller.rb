aa6e2886956f608bdccdea25f17eb64f3c68c8bd
Henri Karhatsu <henri@karhatsu.com>
2020-04-18 09:16:22 +0300

    Show 20 latest accomplishments on coach summary

--- app/controllers/api/coach/summaries_controller.rb
+++ app/controllers/api/coach/summaries_controller.rb
@@ -3,6 +3,6 @@ class Api::Coach::SummariesController < Api::Coach::CoachBaseController
     @players = @team.players.includes(:accomplishments).order('name')
     @task_count = @team.tasks.count
     @latest_tasks = @team.tasks.includes(:accomplishments).order('publish_date DESC').limit(5)
-    @latest_accomplishments = @team.accomplishments.includes(:task, :player).order('created_at DESC').limit(10)
+    @latest_accomplishments = @team.accomplishments.includes(:task, :player).order('created_at DESC').limit(20)
   end
 end
